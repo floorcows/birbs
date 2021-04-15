66ed183bc17c12c0de5e8668226f3b305f4cb66f
FloorCows <merio.cristiano@grenoble-inp.org>
2021-04-15 20:40:51 +0200

    bugfixes

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,39 @@
+use birbs::vectorize;
+
 fn main() {
-    println!("Hello, world!")
-    let a = 3;
+    println!("Hello, world!");
+}
+
+struct Vector_f32 {
+    x: f32,
+    y: f32,
+    z: f32,
+}
+
+struct Vector_i32 {
+    x: i32,
+    y: i32,
+    z: i32,
+}
+
+struct Individuality {}
+
+struct Birb {
+    pos: Vector_f32,
+    spd: Vector_f32,
+    acc: Vector_f32,
+    ind: Individuality,
+}
+
+struct Chunk {
+    size: u32,
+    pos: Vector_i32,
+    birbs: Vec<Birb>,
+    field: [[[Vector_f32; 3]; 3]; 3], /*alloué dynamiquement pour le momenet à voir si c'est bien */
+}
+
+struct World {
+    worldsize: u32,
+    chunks: [[[Chunk; 100]; 100]; 100],
+    birbs: Vec<Birb>,
 }
---
+++ src/vecs.rs
@@ -0,0 +1,15 @@
+pub struct Vector_f32 {
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+}
+
+pub struct Vector_i32 {
+    pub x: i32,
+    pub y: i32,
+    pub z: i32,
+}
+
+pub fn vectorize(a: Vector_i32){
+
+}
