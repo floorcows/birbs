69d9004cf6697e061a23c25b0f6d1fe3e727833c
Elisa876 <elisa.bossard@protonmail.com>
2021-04-16 15:44:17 +0200

    fonctions forces/acceleration

---
+++ src/fonctions forces.rs
@@ -0,0 +1,20 @@
+fn 2_birbs_interaction(birb: Birb, birb2: Birb) -> Vector_f32 {
+    // (birb, voisin) -> force
+
+}
+
+fn attraction_repulsion(distance: f32) -> f32 {
+
+} 
+
+fn orientation(distance: f32, spd1: Vector_f32, spd2: Vector_f32) -> Vector_f32 {
+    
+}
+
+fn interaction_world(Birb) -> Vector_f32 {
+
+}
+
+fn pfd(birb: Birb) -> Birb {
+
+}
--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,12 @@ struct Vector_f32 {
     z: f32,
 }
 
+impl Vector_f32 {
+    fn norm (&self) -> f32 {
+
+    }
+}
+
 struct Vector_i32 {
     x: i32,
     y: i32,
@@ -32,8 +38,44 @@ struct Chunk {
     field: [[[Vector_f32; 100]; 100]; 100], /*alloué dynamiquement pour le momenet à voir si c'est bien */
 }
 
+impl Chunk {
+    fn update_birbs_list(&mut self) ->Chunk{
+        //je sais plus trop la forme optimale pour les methodes d'initialisation
+
+    }
+
+    fn close_chunks(&self) -> Vec<Chunk> {
+        //pour l'instant c'est juste celui dans lequel on est vu qu'il y en a qu'un
+        
+    }
+
+    fn close_birbs(&self) -> Vec<Birb>{
+
+    }
+
+    fn update_birbs_pos(birbs: Vec<Birb>) -> birbs: Vec<Birb>{
+        
+    }
+
+
+}
+
 struct World {
     worldsize: u32,
     chunks: [[[Chunk; worldsize]; worldsize]; worldsize],
     birbs: Vec<Birb>,
+}
+
+impl World {
+    fn loop(&mut?? self) -> World {
+
+    }
+    fn update_birbs_pos(chunks: ????, birbs: Vec<Birb>) -> birbs: Vec<Birb>{
+
+    }
+
+    fn update_birbs_list(chunks:????) -> chunks??? {
+
+    }
+
 }
